4d9efcebc76ab78219d677c9d5c3334df425d4b8
DTMcrae <dtmcrae0@gmail.com>
2024-12-01 13:53:29 -0800

    Updated UI to scale with screen size

--- Assets/Scripts/APIList.cs
+++ Assets/Scripts/APIList.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public static class API
 {
-    public static string Base = "http://localhost:3030/";
+    public static string Base = "http://localhost:3030/"; //"https://four925-group2-unity-api.onrender.com/";
 }
